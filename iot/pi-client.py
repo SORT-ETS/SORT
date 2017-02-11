6fd6c5888430d29fe7be963e3a7d0d48fd614814
Renaud Vincent <renaud.f.v@gmail.com>
2017-02-11 13:10:09 -0500

    PEP8 formating

--- iot/pi-client.py
+++ iot/pi-client.py
@@ -1,5 +1,8 @@
 import base64
+import json
+import httplib
 import picamera
+import re
 
 camera = picamera.PiCamera()
 camera.hflip = True
@@ -10,5 +13,26 @@ with open('picture.png', 'r') as picture:
     picture_data = picture.read()
 
     base64_picture_data = base64.b64encode(picture_data)
-    print base64_picture_data
-    
+
+    conn = httplib.HTTPConnection("159.203.12.120", 5000, timeout=1000)
+
+    headers = {"Content-type": "application/json"}
+
+    body = {"image": "data:image/jpeg;base64," + base64_picture_data}
+    body_str = json.dumps(body)
+
+    conn.request("POST", "/image", body_str, headers)
+
+    response = conn.getresponse()
+    print response.status, response.reason
+
+    response_body = response.read()
+    base64_analysed_data = re.sub("\data:image/jpeg;base64,", "",
+                                  response_body)
+
+    with open("analysed.png", "wb") as analysed_picture:
+        analysed_picture.write(base64.b64decode(base64_analysed_data))
+
+        analysed_picture.close()
+
+    picture.close()
