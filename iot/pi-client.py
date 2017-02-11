866b9d8b5dc20fb79a67950df886bf87422daad3
Renaud Vincent <renaud.f.v@gmail.com>
2017-02-11 08:56:09 -0500

    Reading image post-capture and encode to base64

--- iot/pi-client.py
+++ iot/pi-client.py
@@ -1,5 +1,14 @@
+import base64
 import picamera
 
 camera = picamera.PiCamera()
+camera.hflip = True
 
-camera.capture('testpython.jpg')
+picture = camera.capture('picture.png', 'png')
+
+with open('picture.png', 'r') as picture:
+    picture_data = picture.read()
+
+    base64_picture_data = base64.b64encode(picture_data)
+    print base64_picture_data
+    
