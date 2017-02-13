309c4d22462c1de05bbed2230309a4dca9c64b54
Vincent Fournier <vince@ntfournier.com>
2017-02-12 19:03:16 -0500

    Add a way to save multiple times on server

--- server/server.py
+++ server/server.py
@@ -1,31 +1,48 @@
 import os
 import base64
 import time
+import random
+import string
 from flask import Flask, request, send_file
 from flask_restful import Resource, Api
 
 from subprocess import call
 
 UPLOAD_FOLDER = '../server/images'
-IMAGE_PROCESSING_CMD = []
+RESULT_FOLDER = '../server/results'
+DARKNET_DIR = '/usr/src/darknet'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['IMAGE_PROCESSING_CMD'] = IMAGE_PROCESSING_CMD
+app.config['RESULT_FOLDER'] = RESULT_FOLDER
+app.config['DARKNET_DIR'] = DARKNET_DIR
+
+preId = 0;
+def id_generator():
+    global preId
+    number = "%03d" % preId
+    preId = preId + 1
+    return number + '_' + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
 
 @app.route('/image', methods=['POST'])
 def analyse_image():
         imageData = request.get_json()['image']
 
-        fileLocation = os.path.join(app.config['UPLOAD_FOLDER'], 'image.png')
+        # Generate an id
+        imageId = id_generator() + '.png'
+        fileLocation = os.path.join(app.config['UPLOAD_FOLDER'], imageId)
+        resultLocation = os.path.join(app.config['RESULT_FOLDER'], imageId)
 
+        # Save the image
         f = open(fileLocation, 'wb')
         f.write(base64.b64decode(imageData.split(',')[1]))
         f.close()
 
-        output = call(['./darknet','classifier','predict','cfg/imagenet1k.data','cfg/extraction.cfg','extraction.weights', fileLocation], cwd=r'../darknet');
+        # Call Yolo
+        output = call(['./yolo.sh', app.config['DARKNET_DIR'], fileLocation, resultLocation]);
 
-        i = open(fileLocation, 'rb')
+        # Encode result
+        i = open(resultLocation, 'rb')
         encoded_string = base64.b64encode(i.read())
         i.close()
 
