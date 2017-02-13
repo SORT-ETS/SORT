855daeb23a35130786dd18d099333e0a65d2fda2
Vincent Fournier <vince@ntfournier.com>
2017-02-13 17:26:46 -0500

    Simplify API
    - Remove things that will be in v2

--- server/server.py
+++ server/server.py
@@ -12,41 +12,26 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 """
-@api {post} /api/image  Request Image Analysis 
+@api {post} /api/image  Request Image Analysis
 @apiName  AnalyseImage
 @apiGroup Communication
 
-@apiParam {Object} image Data associated with the image to analyze (DOM reference to the image tag').
+@apiParam {Object} image Data associated with the image to analyze (DOM reference to the image tag).
 
 @apiSuccess {Object}   image                Encoded analyzed image modified with colorised area.
-@apiSuccess {Object[]} residues             List of sectors where trash were found with information on them. return a list of objects that were found in the image.
-@apiSuccess {Object}   bondaries            Represent the rectangle were the trash is in the image
-@apiSuccess {Integer}  bondaries.x1         This value is the horizontal distance from the upper left of the image to the upper left corner of the bondary
-@apiSuccess {Integer}  bondaries.y1         This value is the vertical distance from the upper left of the image to the upper left corner of the bondary
-@apiSuccess {Integer}  bondaries.x2         This value is the horizontal distance from the upper left of the image to the bottom right corner of the bondary
-@apiSuccess {Integer}  bondaries.y2         This value is the vertical distance from the upper left of the image to the bottom right corner of the bondary
-@apiSuccess {Object[]} categories           Represent the different classification the trash can be.
-@apiSuccess {String}   categories.category  This value is the category of the found trash. (List of categories TBD [WIP])
-@apiSuccess {Number}   categories.certitude This value is the percentage of certitude that the category fits the object.
+@apiSuccess {Object[]} residues             List of the residues that were found in the image.
+@apiSuccess {String}   name                 The name of the item.
+@apiSuccess {String}   categories           Represent the different classification the trash can be.
+@apiSuccess {String}   notes                Notes concerning the item.
 
 @apiParamExample {json} Answer-Exemple
 {
     image : object,
     residues : [
         {
-            bondaries  : {x1 : 100, y1 : 105, x2 : 400, y2 : 308},
-            categories : [
-                {category : "trashable", certitude : 0.14},
-                {category : "recyclable", certitude : 0.60}, 
-                {category : "compostable", certitude : 0.26}
-            ]
-        },
-        {
-            bondaries  : {x1 : 108, y1 : 475, x2 : 23, y2 : 502},
-            categories : [
-                {category : "recyclable", certitude : 0.83}, 
-                {category : "compostable", certitude : 0.17}
-            ]
+            name: "container_metro",
+            categories : "recyclable",
+            notes: "May contain something inside, handle carefully"
         }
     ]
 }
