5f98873409aec7e6db0343e5c8c05696a6dfa37f
Vincent Fournier <vince@ntfournier.com>
2017-02-16 14:11:24 -0500

    Change Yolo version
    - Parse boundaries output to Tuples
    - Change Yolo version in Dockerfile
    - Add multithread to server
    - Improve stub to return random boxes of items
    - improve ./darknet call within Yolo.sh server script

--- server/tools/stub.py
+++ server/tools/stub.py
@@ -1,16 +1,21 @@
-from random import random, shuffle
+import random
 
 class Stub():
     possibleItems = ['container_metro', 'can_pepsi', 'can_monster', 'compost', 'dishes']
+    minItemsFound = 1
+    maxItemsFound = 4
 
-    # TODO Add randomization
-    def getRandomOutput(self):
-        shuffle(self.possibleItems)
-
-        output = ""
+    def getRandomOutput(self, imageWidth, imageHeight):
+        boxes = []
         probability = 1
-        for i in self.possibleItems:
-            probability *= random() # Generate decreasing probability
-            output += "{}: {}\n".format(i, probability)
+        for _ in range(random.randrange(self.minItemsFound, self.maxItemsFound)):
+            left = random.randint(0, imageWidth)
+            right = random.randint(left, imageWidth)
 
-        return output
+            # top is lower than bottom because (0,0) is Top left corner.
+            top = random.randint(0, imageHeight)
+            bottom = random.randint(top, imageHeight)
+
+            boxes.append((random.choice(self.possibleItems), left, right, top, bottom))
+
+        return boxes
