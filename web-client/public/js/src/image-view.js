6b8e6e33a125e68e62998240e51f3999fca5ef52
Maxim Daigle <maxim.daigle.1@gmail.com>
2017-03-06 23:15:26 -0500

    Added warnings and show them properly on client

--- web-client/public/js/src/image-view.js
+++ web-client/public/js/src/image-view.js
@@ -58,7 +58,7 @@ export default class ImageView extends View {
 				case "metal":
 				context.strokeStyle="blue";
 				break;
-				case "Composte":
+				case "composte":
 				context.strokeStyle="red";
 				break;
 				default:
