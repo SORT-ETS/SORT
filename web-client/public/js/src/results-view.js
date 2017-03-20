dd7d9bc8a9ced8aa40d7a0cac1f51f26545ac3b2
Renaud Vincent <renaud.f.v@gmail.com>
2017-03-20 15:50:49 -0400

    Stubing results in Analysis, displaying categoreis count dynamically using Handlebars templated fetched with AJAX

--- web-client/public/js/src/results-view.js
+++ web-client/public/js/src/results-view.js
@@ -15,21 +15,23 @@ export default class ResultsView extends View {
 		this.domElement = document.getElementById(this.domId);
 	}
 
-	display(results) {
+	display(resultsTemplate, analysedCategories) {
 		this.domElement.style.display = 'block';
 
-		var resultsTemplate = document.getElementById('results-template');
-		var resultsHTML = this._compileHandlebarsSection(resultsTemplate.innerHTML, results);
-		
-		var resultsContainer = document.getElementById('results-container');
-		resultsContainer.innerHTML = resultsHTML;
+		var resultsHTML = this._compileHandlebarsSection(resultsTemplate, analysedCategories);
+
+		this.domElement.innerHTML = resultsHTML;
 	}
 
 	hide() {
 		this.domElement.style.display = 'none';
+		this.domElement.classList.remove('expanded');
 	}
+
+
 
 	_compileHandlebarsSection(template, data) {
 		return Handlebars.compile(template)(data);
 	}
+
 }
