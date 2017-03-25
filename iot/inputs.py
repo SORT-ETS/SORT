093c923bd82ee2d13ae291d8e6d41656c7094ab5
Renaud Vincent <renaud.f.v@gmail.com>
2017-03-25 16:23:55 -0400

    Four digital inputs triggering up-right-down-left arrows

--- iot/inputs.py
+++ iot/inputs.py
@@ -15,21 +15,55 @@ GPIO.setmode(GPIO.BCM)
 
 # IO inputs setup
 buttonPin = 21
+upPin = 26
+rightPin = 19
+downPin = 13
+leftPin = 6
+
 GPIO.setup(buttonPin, GPIO.IN)
+GPIO.setup(upPin, GPIO.IN)
+GPIO.setup(rightPin, GPIO.IN)
+GPIO.setup(downPin, GPIO.IN)
+GPIO.setup(leftPin, GPIO.IN)
 
 # bouncing setup
 prev_input = 0
+prev_up = 0
+prev_right = 0
+prev_down = 0
+prev_left = 0
 
 while True:
-    input = GPIO.input(buttonPin)
+    inputButton = GPIO.input(buttonPin)
+    inputUp = GPIO.input(upPin)
+    inputRight = not GPIO.input(rightPin)  # needed to inverse
+    inputDown = GPIO.input(downPin)
+    inputLeft = not GPIO.input(leftPin)  # needed to inverse
 
-    if ((not prev_input) and input):
-        print('Pressed')
+    if ((not prev_input) and inputButton):
         keyboard.press_and_release('enter')
+
         bus.write_byte_data(address, 0, on_sig)  # Dels on
         time.sleep(0.5)  # to get a long enough flash
         bus.write_byte_data(address, 0, off_sig)  # Dels off
 
-    prev_input = input
+    if ((not prev_up) and inputUp):
+        keyboard.press_and_release('up arrow')
 
-    time.sleep(0.05)  # for bouncing purposes
+    if ((not prev_right) and inputRight):
+        keyboard.press_and_release('right arrow')
+
+    if ((not prev_down) and inputDown):
+        keyboard.press_and_release('down arrow')
+
+    if ((not prev_left) and inputLeft):
+        keyboard.press_and_release('left arrow')
+
+    # for bouncing purposes
+    prev_input = inputButton
+    prev_up = inputUp
+    prev_right = inputRight
+    prev_down = inputDown
+    prev_left = inputLeft
+
+    time.sleep(0.05)
