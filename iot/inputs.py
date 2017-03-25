f9f61d1a2008b448cec3752105da17c7e9854327
Renaud Vincent <renaud.f.v@gmail.com>
2017-03-25 13:23:55 -0400

    Circuit fix, activating brightpi on input pep8 formated

--- iot/inputs.py
+++ iot/inputs.py
@@ -13,22 +13,21 @@ off_sig = 0x00
 GPIO.setmode(GPIO.BCM)
 
 # IO inputs setup
-buttonPullUpPin = 21
-GPIO.setup(buttonPullUpPin, GPIO.IN)
+buttonPin = 21
+GPIO.setup(buttonPin, GPIO.IN)
 
-#bouncing setup
+# bouncing setup
 prev_input = 0
 
 while True:
-    input = GPIO.input(buttonPullUpPin)
-    print input, prev_input
+    input = GPIO.input(buttonPin)
+
     if ((not prev_input) and input):
-        print 'Pressed'
-        bus.write_byte_data(address, 0, on_sig) # Dels on
-        time.sleep(0.5) # to get a long enough flash
-        bus.write_byte_data(address, 0, off_sig) # Dels off
-        
+        print('Pressed')
+        bus.write_byte_data(address, 0, on_sig)  # Dels on
+        time.sleep(0.5)  # to get a long enough flash
+        bus.write_byte_data(address, 0, off_sig)  # Dels off
+
     prev_input = input
 
-    time.sleep(0.1) # for bouncing purposes
-    
+    time.sleep(0.05)  # for bouncing purposes
