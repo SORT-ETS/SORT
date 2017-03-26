9bc8543c14a365fa223e8268118b5565f37ad65b
Vincent Fournier <vince@ntfournier.com>
2017-03-26 12:46:46 -0400

    Always get newest configuration

--- server/tools/yolo.sh
+++ server/tools/yolo.sh
@@ -4,6 +4,6 @@ FILE_LOCATION=$2
 RESULT_LOCATION=$3
 
 cd $DARKNET_DIR
-./darknet detect yolo-sort.cfg backup/yolo-sort.weights $FILE_LOCATION 2> /dev/null | grep 'box' | sed -e "s/^box://"
+./darknet detect yolo-sort.cfg yolo-sort.weights $FILE_LOCATION 2> /dev/null | grep 'box' | sed -e "s/^box://"
 
 mv predictions.png $RESULT_LOCATION
