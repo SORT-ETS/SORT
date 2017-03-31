a8377c0468b48af2f67d0a6fe244c82a053cbdf4
Vincent Fournier <vince@ntfournier.com>
2017-03-31 17:01:18 -0400

    Improve stub and fix server bug

--- server/tools/stub.py
+++ server/tools/stub.py
@@ -7,17 +7,21 @@ class Stub():
                      'container_bag', 'compost', 'plastic_bottle']
     minItemsFound = 3
     maxItemsFound = 7
+    minWidth = 80
+    minHeight = 90
+    minTop = 50
+    maxOffsetBottom = 135
 
     def getRandomOutput(self, imageWidth, imageHeight):
         boxes = []
         nbOutputs = random.randrange(self.minItemsFound, self.maxItemsFound)
         for _ in range(nbOutputs):
-            left = random.randint(0, imageWidth)
-            right = random.randint(left, imageWidth)
+            left = random.randint(0, imageWidth - self.minWidth)
+            right = random.randint(left + self.minWidth, imageWidth)
 
             # top is lower than bottom because (0,0) is Top left corner.
-            top = random.randint(0, imageHeight)
-            bottom = random.randint(top, imageHeight)
+            top = random.randint(self.minTop, imageHeight - self.maxOffsetBottom - self.minHeight)
+            bottom = random.randint(top + self.minHeight, imageHeight - self.maxOffsetBottom)
 
             boxes.append((random.choice(self.possibleItems),
                           left, right,
